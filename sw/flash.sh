9a79561c812cbbd6d634d251b0e106b887130bf4
IshDeshpa <ishdeshpa@gmail.com>
2025-03-31 13:32:48 -0500

    driverlib added, testing basic spi transactions

--- sw/flash.sh
+++ sw/flash.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Get the current directory
+Root="$(pwd)"
+builddir="build/"
+
 ./build.sh
 #openocd -f ti_ek-tm4c123gxl.cfg -c "program build/exe.elf verify reset exit"
 
 echo -e "\nflashing...\n"
 # Flash the binary to the microcontroller
-lm4flash "${Root}/${builddir}exe.bin"
+lm4flash -E "${Root}/${builddir}exe.bin"
--- sw/nm/nm_bsp.h
+++ sw/nm/nm_bsp.h
@@ -31,285 +31,63 @@
  * \asf_license_stop
  *
  */
-/**@defgroup  BSPDefine Defines
- * @ingroup nm_bsp
- * @{
- */
 #ifndef _NM_BSP_H_
 #define _NM_BSP_H_
 
-#define NMI_API
-/*!< 
-*        Attribute used to define memory section to map Functions in host memory.
-*/
+#include <stdint.h>
 
-#ifdef __MCF964548__
-#define CONST code
-#else
-#define CONST const
-#endif
-/*!< 
-*     Used for code portability.
-*/
+#include "../inc/tm4c123gh6pm.h"
 
-/*!
- * @typedef      void (*tpfNmBspIsr) (void);
- * @brief           Pointer to function.\n
- *                     Used as a data type of ISR function registered by \ref nm_bsp_register_isr
- * @return         None
+#define WILC_DFL_CTRL_PORT (GPIO_PORTC_DATA_BITS_R)
+
+// #define WILC_CS_PORT (WILC_DFL_CTRL_PORT)
+// #define WILC_CS_PIN (1<<4)
+
+#define WILC_RESET_PORT (WILC_DFL_CTRL_PORT)
+#define WILC_RESET_PIN (1<<5)
+
+#define WILC_CHIPEN_PORT (WILC_DFL_CTRL_PORT)
+#define WILC_CHIPEN_PIN (1<<6)
+
+#define WILC_WAKE_PORT (WILC_DFL_CTRL_PORT)
+#define WILC_WAKE_PIN (1<<7)
+
+// #define WILC_CS_HI (*(WILC_CS_PORT + WILC_CS_PIN) = WILC_CS_PIN)
+// #define WILC_CS_LO (*(WILC_CS_PORT + WILC_CS_PIN) &= ~WILC_CS_PIN)
+
+#define WILC_RESET_HI (*(WILC_RESET_PORT + WILC_RESET_PIN) = WILC_RESET_PIN)
+#define WILC_RESET_LO (*(WILC_RESET_PORT + WILC_RESET_PIN) &= ~WILC_RESET_PIN)
+
+#define WILC_CHIPEN_HI (*(WILC_CHIPEN_PORT + WILC_CHIPEN_PIN) = WILC_CHIPEN_PIN)
+#define WILC_CHIPEN_LO (*(WILC_CHIPEN_PORT + WILC_CHIPEN_PIN) &= ~WILC_CHIPEN_PIN)
+
+#define WILC_WAKE_HI (*(WILC_WAKE_PORT + WILC_WAKE_PIN) = WILC_WAKE_PIN)
+#define WILC_WAKE_LO (*(WILC_WAKE_PORT + WILC_WAKE_PIN) &= ~WILC_WAKE_PIN)
+
+/* 
+ * Initializes the Board Support Package (BSP), including reset and chip enable pins for WILC, delays, 
+ * registering ISR, and enabling/disabling IRQ for WILC. This function must be called at the start of 
+ * the application to enable communication between WILC and the host driver.
+ * Returns M2M_SUCCESS on success, or a negative value on failure.
  */
-typedef void (*tpfNmBspIsr)(void);
+int8_t nm_bsp_init(void);
 
-
-
-#ifndef NULL
-#define NULL ((void*)0)
-#endif
-/*!< 
-*    Void Pointer to '0' in case of NULL is not defined. 
-*/
-
-
-#define BSP_MIN(x,y) ((x)>(y)?(y):(x))
-/*!< 
-*     Computes the minimum of \b x and \b y.
-*/
-
- //@}
-
-/**@defgroup  DataT  DataTypes
- * @ingroup nm_bsp
- * @{
+/* 
+ * Deinitializes the BSP. This function should be called after nm_bsp_init. Missing this call may lead 
+ * to undefined behavior during a soft reset. Returns M2M_SUCCESS on success, or a negative value on failure.
  */
- 
-  /*!
- * @ingroup DataTypes
- * @typedef      unsigned char	uint8;
- * @brief        Range of values between 0 to 255
- */
-typedef unsigned char	uint8;
+int8_t nm_bsp_deinit(void);
 
- /*!
- * @ingroup DataTypes
- * @typedef      unsigned short	uint16;
- * @brief        Range of values between 0 to 65535
- */
-typedef unsigned short	uint16;
-
- /*!
- * @ingroup Data Types
- * @typedef      unsigned long	uint32;
- * @brief        Range of values between 0 to 4294967295
- */ 
-typedef unsigned long	uint32;
-
-
-  /*!
- * @ingroup Data Types
- * @typedef      signed char		sint8;
- * @brief        Range of values between -128 to 127
- */
-typedef signed char		sint8;
-
- /*!
- * @ingroup DataTypes
- * @typedef      signed short	sint16;
- * @brief        Range of values between -32768 to 32767
- */
-typedef signed short	sint16;
-
-  /*!
- * @ingroup DataTypes
- * @typedef      signed long		sint32;
- * @brief        Range of values between -2147483648 to 2147483647
- */
-
-typedef signed long		sint32;
- //@}
-
-#ifndef CORTUS_APP
-
-
-#ifdef __cplusplus
-extern "C"{
-#endif
-
-/** \defgroup BSPAPI Function
- *   @ingroup nm_bsp
- */
-
-
-/** @defgroup NmBspInitFn nm_bsp_init
- *  @ingroup BSPAPI
- *   Initialize BSP
- */
- /**@{*/
-/*!
- * @fn           sint8 nm_bsp_init(void);
- * @brief        Initialization for bsp (\e Board \e Support \e Package) such as Reset and Chip Enable Pins for WILC, delays, register ISR, enable/disable IRQ for WILC, ...etc.\n
- *                   You must use this function in the head of your application to enable WILC and Host Driver communicate each other.
- * @warning      Missing use will lead to unavailability of host communication.\n
- *  @note        Implementation of this function is host dependent.
- * @return       The function returns @ref M2M_SUCCESS for successful operations and a negative value otherwise.
-
- */
-sint8 nm_bsp_init(void);
- /**@}*/
-
- 
- /** @defgroup NmBspDeinitFn nm_bsp_deinit
- *    @ingroup BSPAPI
- *   	 De-initialization for bsp (\e Board \e Support \e Package)
- */
- /**@{*/
-/*!
- * @fn           sint8 nm_bsp_deinit(void);
- * @pre          Initialize \ref nm_bsp_init first
- * @warning      Missing use may lead to unknown behavior in case of soft reset.\n
- * @note         Implementation of this function is host dependent.
- * @see          nm_bsp_init               
- * @return      The function returns @ref M2M_SUCCESS for successful operations and a negative value otherwise.
-
- */
-sint8 nm_bsp_deinit(void);
- /**@}*/
-
- 
-/** @defgroup NmBspResetFn  nm_bsp_reset
-*     @ingroup BSPAPI
-*      Reinitialization for bsp (\e Board \e Support \e Package) by resetting NMC1500 SoC by setting \ref CHIP_EN and \ref RESET_N signals low, 
- *     \ref CHIP_EN high then \ref RESET_N high
-*/
-/**@{*/
- /*!
- * @fn           void nm_bsp_reset(void);
- * @brief     
- * @param [in]   None
- * @pre          Initialize \ref nm_bsp_init first
- * @note         Implementation of this function is host dependent and called by HIF layer.
- * @see          nm_bsp_init    
- * @return       None
-
+/* 
+ * Resets the NMC1500 SoC by toggling the CHIP_EN and RESET_N signals. This function is host-dependent 
+ * and is called by the HIF layer. Ensure nm_bsp_init is called before using this function.
  */
 void nm_bsp_reset(void);
- /**@}*/
 
- 
-/** @defgroup NmBspSleepFn nm_bsp_sleep
-*     @ingroup BSPAPI
-*     Sleep in units of milliseconds.\n
-*    This function used by HIF Layer according to different situations. 
-*/
-/**@{*/
-/*!
- * @fn           void nm_bsp_sleep(uint32);
- * @brief   
- * @param [in]   u32TimeMsec
- *               Time unit in milliseconds
- * @pre          Initialize \ref nm_bsp_init first
- * @warning      Maximum value must nor exceed 4294967295 milliseconds which is equal to 4294967.295 seconds.\n
- * @note         Implementation of this function is host dependent.
- * @see           nm_bsp_init               
- * @return       None
+/* 
+ * Puts the system to sleep for the specified time in milliseconds. The maximum value is 4294967295 ms 
+ * (approximately 4294967.295 seconds). This function is host-dependent and is used by the HIF layer.
  */
-void nm_bsp_sleep(uint32 u32TimeMsec);
-/**@}*/
-
-  
-/** @defgroup NmBspRegisterFn nm_bsp_register_isr
-*     @ingroup BSPAPI
-*   Register ISR (\e Interrupt \e Service \e Routine) in the initialization of HIF (\e Host \e Interface) Layer.
-*/
-/**@{*/
-/*!
- * @fn           void nm_bsp_register_isr(tpfNmBspIsr);
- * @param [in]   pfIsr
- *               Pointer to ISR handler in HIF
- * @warning      Make sure that ISR for IRQ pin for WILC is disabled by default in your implementation.
- * @note         Implementation of this function is host dependent and called by HIF layer.
-  * @see        tpfNmBspIsr
- * @return       None
-
- */
-void nm_bsp_register_isr(tpfNmBspIsr pfIsr);
-/**@}*/
-
-  
-/** @defgroup NmBspInterruptCtrl nm_bsp_interrupt_ctrl
-*     @ingroup BSPAPI
-*    Synchrnonous enable/disable interrupts fucntion
-*/
-/**@{*/
-/*!
- * @fn           void nm_bsp_interrupt_ctrl(uint8);
- * @brief        Enable/Disable interrupts
- * @param [in]   u8Enable
- *               '0' disable interrupts. '1' enable interrupts 
- * @see          tpfNmBspIsr           
- * @note         Implementation of this function is host dependent and called by HIF layer.
- * @return       None
-
- */
-void nm_bsp_interrupt_ctrl(uint8 u8Enable);
-  /**@}*/
-
- /** @defgroup NmBspMalloc nm_bsp_malloc
-*     @ingroup BSPAPI
-*    Synchrnonous memory allocate API
-*/
-/**@{*/
-/*!
- * @fn           void* nm_bsp_malloc(uint32);
- * @brief        Allocate memory
- * @param [in]   u32Size
- *               Size of the requested memory 
- * @note         Implementation of this function is host dependent and called by HIF layer.
- * @return       Pointer to the allocated buffer, or NULL otherwise
-
- */
-void* nm_bsp_malloc(uint32 u32Size);
-  /**@}*/
-
- /** @defgroup NmBspFree nm_bsp_free
-*     @ingroup BSPAPI
-*    Synchrnonous memory free API
-*/
-/**@{*/
-/*!
- * @fn           void nm_bsp_free(void*);
- * @brief        Free memory
- * @param [in]   pvMemBuffer
- *               Pointer to the buffer to be freed 
- * @note         Implementation of this function is host dependent and called by HIF layer.
- * @return       None
-
- */
-void nm_bsp_free(void* pvMemBuffer);
-  /**@}*/
- 
-#ifdef __cplusplus
-}
-#endif
+void nm_bsp_sleep(uint32_t u32TimeMsec);
 
 #endif
-
-#include <bsp/include/nm_bsp_pic32.h>
-#define __M2M_DMA_BUF_ATT__
-
-
-#ifdef _NM_BSP_BIG_END
-#define NM_BSP_B_L_32(x) \
-((((x) & 0x000000FF) << 24) + \
-(((x) & 0x0000FF00) << 8)  + \
-(((x) & 0x00FF0000) >> 8)   + \
-(((x) & 0xFF000000) >> 24))
-#define NM_BSP_B_L_16(x) \
-((((x) & 0x00FF) << 8) + \
-(((x)  & 0xFF00) >> 8))
-#else
-#define NM_BSP_B_L_32(x)  (x)
-#define NM_BSP_B_L_16(x)  (x)
-#endif
-
-
-#endif	/*_NM_BSP_H_*/
