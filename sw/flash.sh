d1c38b4f0445e194c46321aa4a86d89d06db2812
maanavkoladia <maanav.koladia@gmail.com>
2025-04-17 00:42:32 -0500

    bear

--- sw/flash.sh
+++ sw/flash.sh
@@ -3,8 +3,9 @@
 builddir="build/"
 NUM_CORES=$(nproc) # On Linux
 
-make clean
-make flash -j$NUM_CORES -s -B
+bear -- make clean
+bear -- make flash -j$NUM_CORES -s -B
+mv compile_commands.json build/
 #make dump
 echo -e "Used $NUM_CORES for buidling\n"
 echo -e "Flashing to Target..."
