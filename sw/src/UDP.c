6502f67101c32dcdb9b3bf388e1baf48ba9a6f7e
IshDeshpa <ishdeshpa@gmail.com>
2025-04-22 18:31:54 -0500

    udp finish!

--- sw/src/UDP.c
+++ sw/src/UDP.c
@@ -1,12 +1,31 @@
 #include "UDP.h"
+#include "ip.h"
 #include "Networking_Globs.h"
+#include "string_lite.h"
 
 #define SOURCE_PORT (0xBEEF)
 #define CHECKSUM    (0x0000)
 
-// data should point to
-int UDP_TX(uint8_t *data, uint8_t size, uint16_t port){
-    ASSERT(data == curr_packet_buffer);
-    
-    
+typedef struct __attribute__((packed)) {
+    uint16_t sourcePort;        // Source Port
+    uint16_t destinationPort;   // Destination Port
+    uint16_t length;            // Length of UDP header + data
+    uint16_t checksum;          // Checksum
+} udpHeader_t;
+
+int udp_tx(uint8_t payloadsize, uint8_t *payload, uint32_t destinationIP, uint16_t port){
+    ASSERT(payload == curr_packet_buffer);
+    ASSERT(payloadsize <= MTU);
+
+    // Move for UDP header
+    memmove(payload + sizeof(udpHeader_t), payload, payloadsize);
+
+    udpHeader_t* header = (udpHeader_t*)payload;
+    header->sourcePort = SOURCE_PORT;
+    header->destinationPort = port;
+    header->length = payloadsize + sizeof(udpHeader_t);
+    ASSERT(header->length >= sizeof(udpHeader_t));
+    header->checksum = CHECKSUM;
+
+    ip4_tx(payloadsize + sizeof(udpHeader_t), payload, IP_PROTOCOL_UDP, destinationIP);
 }
--- sw/src/UDP.h
+++ sw/src/UDP.h
@@ -1,4 +1,10 @@
 #ifndef UDP_H
 #define UDP_H
 
+typedef enum {
+    UDP_SUCCESS,
+    UDP_TX_FAIL,
+    UDP_RX_FAIL,
+}errUDP_t;
+
 #endif
