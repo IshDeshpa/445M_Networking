095bb64d07fe23922fd3ed237361b1c77b4b5780
maanavkoladia <maanav.koladia@gmail.com>
2025-04-23 17:08:45 -0500

    reacreted th seg fuatl

--- sw/testingProtocol/main.c
+++ sw/testingProtocol/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include "UDP.h"
 #include "ip.h"
 #include "Networking_Globs.h"
 #include "stubs.h"
@@ -23,7 +24,7 @@ int main(int argc, char *argv[]){
     LOG("Starting up testing\n\r");
     memcpy(networkBuf, payload, PAYLOAD_SIZE);
     //ethernetTX(payload, PAYLOAD_SIZE);
-    int ret = ip4_tx(PAYLOAD_SIZE, networkBuf, IP_PROTOCOL_UDP, DESTINATION_IP);
+    int ret = udp_tx(PAYLOAD_SIZE, networkBuf, DESTINATION_IP, 4567);
     if(ret == IP_SUCCESS){
         printf("Ip sent succesfully\n");
     }else{
